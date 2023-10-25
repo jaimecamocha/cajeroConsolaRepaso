cfdc820b274d93d31f06f8daff52c28d4a567916
Jaime Fernandez <jaimefernandezcalvo@alumnos.tuniversformacion.es>
2023-10-25 08:46:18 +0200

    Declaramos las funciones

---
+++ index.js
@@ -0,0 +1,30 @@
+// JAIME FERNÁNDEZ CALVO
+//
+
+let saldo = 1000;
+const PIN_CORRECTO = "1234";
+let intentosRestantes = 3;
+
+function mostrarSaldo(){
+
+}
+
+function depositar(){
+
+}
+
+function retirar(){
+
+}
+
+function transferir(){
+
+}
+
+function iniciarSesion(){
+
+}
+
+function operacionesCajero(){
+    
+}
