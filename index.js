27738939fe5e12400005ddbdedfadc5cf35534b7
Jaime Fernandez <jaimefernandezcalvo@alumnos.tuniversformacion.es>
2023-10-25 09:29:52 +0200

    Hacemos la función de mostrar el saldo

--- index.js
+++ index.js
@@ -6,15 +6,27 @@ const PIN_CORRECTO = "1234";
 let intentosRestantes = 3;
 
 function mostrarSaldo(){
-
+    console.log(`Su saldo actual es: ${saldo.toFixed(2)} €`);
 }
 
 function depositar(){
-
+    const deposito = parseFloat(prompt("Ingrese la cantidad de depositar: "));
+    if(isNaN(deposito) || deposito <= 0){
+        console.log("Cantidad inválida. Inténtelo de nuevo.")
+    } else{
+        saldo = saldo + deposito;
+        console.log(`Se han depositado ${deposito.toFixed(2)} €`);
+    }
 }
 
 function retirar(){
-
+    const retiro = parseFloat(prompt("Ingrese la cantidad de retirar: "));
+    if(isNaN(retiro) || retiro <= 0){
+        console.log("Cantidad inválida. Inténtelo de nuevo.")
+    } else{
+        saldo = saldo - retiro;
+        console.log(`Se han retirado ${retiro.toFixed(2)} €`);
+    }
 }
 
 function transferir(){
@@ -26,5 +38,7 @@ function iniciarSesion(){
 }
 
 function operacionesCajero(){
-    
+
 }
+
+iniciarSesion();
