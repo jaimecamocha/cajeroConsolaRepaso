d975613282ca5920278a1fef1dab56e044ed34a5
Jaime Fernandez <jaimefernandezcalvo@alumnos.tuniversformacion.es>
2023-10-25 09:38:33 +0200

    Hacemos la función de depositar y la de retirar diner

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ let saldo = 1000;
 const PIN_CORRECTO = "1234";
 let intentosRestantes = 3;
 
+ // mostrar saldo
 function mostrarSaldo(){
     console.log(`Su saldo actual es: ${saldo.toFixed(2)} €`);
 }
 
+// depositar dinero
 function depositar(){
     const deposito = parseFloat(prompt("Ingrese la cantidad de depositar: "));
     if(isNaN(deposito) || deposito <= 0){
@@ -17,11 +19,12 @@ function depositar(){
         saldo = saldo + deposito;
         console.log(`Se han depositado ${deposito.toFixed(2)} €`);
     }
-} 
+}
 
+// retirar dinero
 function retirar(){
     const retiro = parseFloat(prompt("Ingrese la cantidad de retirar: "));
-    if(isNaN(retiro) || retiro <= 0){
+    if(isNaN(retiro) || retiro <= 0 || retiro > saldo){
         console.log("Cantidad inválida. Inténtelo de nuevo.")
     } else{
         saldo = saldo - retiro;
@@ -29,16 +32,26 @@ function retirar(){
     }
 }
 
+// transferir dinero
 function transferir(){
-
+    const monto = parseFloat(prompt("Ingrese la cantidad a transferir: "));
+    if(isNaN(monto) || monto <= 0 || monto > saldo){
+        console.log("Cantidad inválida. Inténtelo de nuevo.")  
+    } else{
+        saldo = saldo - monto;
+        console.log(`Se han transferido ${monto.toFixed(2)} €`);        
+    }
 }
 
+// iniciar sesión
 function iniciarSesion(){
 
 }
 
+// operaciones del cajero
 function operacionesCajero(){
 
 }
 
+// llamada  la función de inicio de sesión
 iniciarSesion();
